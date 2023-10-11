f6ecc5af22b89bd4b08800e75c55e29f04ec5949
Bipul <mhbipul99bd@gmail.com>
2023-10-11 17:43:21 +0600

    class-01

---
+++ src/app/account/login/page.tsx
@@ -0,0 +1,7 @@
+export default function page(){
+    return(
+        <div>
+            <h1>This is a page under login under Account</h1>
+        </div>
+    )
+}
---
+++ src/app/account/page/page.tsx
@@ -0,0 +1,8 @@
+export default function page(){
+    return(
+        <div>
+        <h1> This is a page file under page folder,under account folder!
+        </h1>
+        </div>
+    )
+}
